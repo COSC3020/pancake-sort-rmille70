97534721a4326b5bbff07a1e7156f519fda3dc45
Ryan Miller <rmille70@uwyo.edu>
2024-04-20 12:57:44 -0600

    Added References

--- code.js
+++ code.js
@@ -1,3 +1,7 @@
+// References:
+// https://en.wikipedia.org/wiki/Pancake_sorting#:~:text=The%20original%20pancake%20problem,-The%20minimum%20number&text=The%20simplest%20pancake%20sorting%20algorithm,process%20for%20the%20remaining%20pancakes.
+// https://javascript.plainenglish.io/pancake-sorting-in-javascript-ecc1486e7e33
+
 function flip(array, n) {
     let hi = n;
     let lo = 0;
