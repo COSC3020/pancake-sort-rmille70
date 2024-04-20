ceab14b07c95203148a4594fecf1d660f7665821
Ryan Miller <rmille70@uwyo.edu>
2024-04-20 12:55:41 -0600

    Completed Code + Analysis

--- code.js
+++ code.js
@@ -1,8 +1,30 @@
 function flip(array, n) {
+    let hi = n;
+    let lo = 0;
+    if(hi > array.length-1){ hi = array.length-1; }
+    while(lo <= hi){
+        let tmp = array[lo];
+        array[lo] = array[hi];
+        array[hi] = tmp;
+        lo++;
+        hi--;
+    }
     return array;
 }
 
-// Use only flip() here to manipulate the array
 function pancakeSort(array) {
+    let unsorted = array.length-1;
+    while(unsorted >= 0){
+        let maxInd = 0;
+        for(let j = 0; j <= unsorted; j++) {
+            if(array[j] > array[maxInd]) {
+                maxInd = j;
+            }
+        }
+        array = flip(array, maxInd);
+        array = flip(array, unsorted);
+        unsorted--;
+    }
     return array;
 }
+
